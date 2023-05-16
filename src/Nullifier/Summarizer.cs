3ee17f342f2a3eaec761f52be285ae5ebf5c1e16
Bill Menees <Bill@Menees.com>
2023-05-16 07:27:53 -0500

    Summarize by folder

--- src/Nullifier/Summarizer.cs
+++ src/Nullifier/Summarizer.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using Menees;
 
 #endregion
 
@@ -33,32 +34,47 @@ internal sealed class Summarizer
 
 	public void Summarize()
 	{
-		foreach (var fileGroup in this.problems.GroupBy(problem => problem.File).OrderBy(g => g.Count()).ThenBy(g => g.Key))
+		foreach (var folderGroup in this.problems.GroupBy(problem => Path.GetDirectoryName(problem.File)).OrderBy(g => g.Count()).ThenBy(g => g.Key))
 		{
-			string name = fileGroup.Key[(this.arguments.ProjectDirectory?.Length ?? 0)..]
-				.TrimStart(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+			string folder = this.TrimProjectDirectory(folderGroup.Key ?? string.Empty);
+			Console.WriteLine($"{(folder.IsWhiteSpace() ? "<Root>" : folder)}: {folderGroup.Count()}");
 
-			Console.Write($"{name}: {fileGroup.Count():N0} [");
-
-			bool first = true;
-			foreach (var codeGroup in fileGroup.GroupBy(problem => problem.Code).OrderBy(g => g.Count()).ThenBy(g => g.Key))
+			foreach (var fileGroup in folderGroup.GroupBy(problem => problem.File).OrderBy(g => g.Count()).ThenBy(g => g.Key))
 			{
-				if (!first)
+				string name = this.TrimProjectDirectory(fileGroup.Key);
+				Console.Write($"  {name}: {fileGroup.Count():N0} [");
+
+				bool first = true;
+				foreach (var codeGroup in fileGroup.GroupBy(problem => problem.Code).OrderBy(g => g.Count()).ThenBy(g => g.Key))
 				{
-					Console.Write("; ");
+					if (!first)
+					{
+						Console.Write("; ");
+					}
+
+					Console.Write($"{codeGroup.Key}={codeGroup.Count():N0}");
+					first = false;
 				}
 
-				Console.Write($"{codeGroup.Key}={codeGroup.Count():N0}");
-				first = false;
+				Console.WriteLine("]");
 			}
-
-			Console.WriteLine("]");
 		}
 
 		if (this.problems.Count > 0)
 		{
 			Console.WriteLine("-----------------------------------------------");
 		}
+	}
+
+	#endregion
+
+	#region Private Methods
+
+	private string TrimProjectDirectory(string path)
+	{
+		string result = path[(this.arguments.ProjectDirectory?.Length ?? 0)..]
+			.TrimStart(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+		return result;
 	}
 
 	#endregion
